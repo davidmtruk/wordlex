62c9c26f5108918857b0d647db28d357db0894cc
davidmtruk <160794490+davidmtruk@users.noreply.github.com>
2024-03-02 12:11:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const qwertyLetters = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-        ["Z", "X", "C", "V", "B", "N", "M"]
+        ["Z", "X", "C", "V", "B", "N", "M","⌫"]
     ];
 
     // Generar teclado virtual
